8a620dc401fe29b6c4f0fd854c7e7d8c00f7dd5a
Gulmira <gulmiraisakbaeva78@gmail.com>
2024-09-05 14:50:20 +0600

    Миграция данных с одной бд в другую в джанго

--- core/settings.py
+++ core/settings.py
@@ -72,10 +72,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-#
+# #
 # # Database
 # # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
